cbc3c84dcd5b3d7e8ee022be7ce38b87a2ef86ed
Sean Boling <sean.boling@gmail.com>
2023-04-24 16:34:51 -0600

    removed / at end of URLs in whitelist

--- config/whitelist.js
+++ config/whitelist.js
@@ -2,8 +2,8 @@ const allowedOrigins = [
     'https://www.yoursite.com',
     'http://127.0.0.1:3500',
     'http://loclahost:3500',
-    'http://http://faithful-little-trombone.glitch.me/',
-    'https://dazzling-snickerdoodle-777101.netlify.app/'
+    'http://http://faithful-little-trombone.glitch.me',
+    'https://dazzling-snickerdoodle-777101.netlify.app'
 ];
 
 module.exports = allowedOrigins;
