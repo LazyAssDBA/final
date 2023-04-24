3a34da0c577ad3e7a9afa31e2b0572710209dc3b
Sean Boling <sean.boling@gmail.com>
2023-04-24 15:30:11 -0600

    whitelisted site on glitch.me

--- config/whitelist.js
+++ config/whitelist.js
@@ -2,6 +2,7 @@ const allowedOrigins = [
     'https://www.yoursite.com',
     'http://127.0.0.1:3500',
     'http://loclahost:3500',
+    'http://http://faithful-little-trombone.glitch.me/',
     'https://dazzling-snickerdoodle-777101.netlify.app/'
 ];
 
