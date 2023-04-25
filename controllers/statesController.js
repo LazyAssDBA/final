e40bd9f40bfc992a00763c76caefc5ea9a47c0bd
Sean Boling <sean.boling@gmail.com>
2023-04-24 19:39:18 -0600

    undoing broken code after last commit

--- controllers/statesController.js
+++ controllers/statesController.js
@@ -135,29 +135,6 @@ const deleteFunFact = async (req, res) => {
     }
 }
 
-const getFunFacts = async (req, res) => {
-    let code = req.params.code;
-    code = code.toUpperCase();
-    for(x = 0; x < data.states.length; x++) {
-        let array = Object.entries(data.states).map(([key,value])=>value);
-        if(code == array[x].code){
-            var facts = await State.findOne({ stateCode: code }).exec();
-            var result = data.states.filter(obj=> obj.code == code);
-            var updatedState = result[0].state;
-            if(facts != null) {
-                var resultObject = { funfacts: facts.funfacts };
-                var resultIndex = Math.floor(Math.random() * resultObject.funfacts.length);
-                var funfact = resultObject.funfacts[resultIndex];
-                var updatedReturn = { funfact };
-                
-                return res.json(updatedReturn);
-            }  
-            return res.json({"message": `No Fun Facts found for ${updatedState}`});
-        }
-    } 
-    return res.json({"message":"Invalid state abbreviation parameter"});
-}
-
 module.exports = {
     getAllStates,
     getState,
@@ -166,77 +143,5 @@ module.exports = {
     getPopulation,
     getAdmission,
     createNewFunFacts,
-    deleteFunFact,
-    getFunFacts
+    deleteFunFact
 }
-
-/*
-const Employee = require('../model/Employee');
-
-const getAllEmployees = async (req, res) => {
-    const employees = await Employee.find();
-    if (!employees) return res.status(204).json({ 'message': 'No employees found.' });
-    res.json(employees);
-}
-
-const createNewEmployee = async (req, res) => {
-    if (!req?.body?.firstname || !req?.body?.lastname) {
-        return res.status(400).json({ 'message': 'First and last names are required' });
-    }
-
-    try {
-        const result = await Employee.create({
-            firstname: req.body.firstname,
-            lastname: req.body.lastname
-        });
-
-        res.status(201).json(result);
-    } catch (err) {
-        console.error(err);
-    }
-}
-
-const updateEmployee = async (req, res) => {
-    if (!req?.body?.id) {
-        return res.status(400).json({ 'message': 'ID parameter is required.' });
-    }
-
-    const employee = await Employee.findOne({ _id: req.body.id }).exec();
-    if (!employee) {
-        return res.status(204).json({ "message": `No employee matches ID ${req.body.id}.` });
-    }
-    if (req.body?.firstname) employee.firstname = req.body.firstname;
-    if (req.body?.lastname) employee.lastname = req.body.lastname;
-    const result = await employee.save();
-    res.json(result);
-}
-
-const deleteEmployee = async (req, res) => {
-    if (!req?.body?.id) return res.status(400).json({ 'message': 'Employee ID required.' });
-
-    const employee = await Employee.findOne({ _id: req.body.id }).exec();
-    if (!employee) {
-        return res.status(204).json({ "message": `No employee matches ID ${req.body.id}.` });
-    }
-    const result = await employee.deleteOne(); //{ _id: req.body.id }
-    res.json(result);
-}
-
-const getEmployee = async (req, res) => {
-    if (!req?.params?.id) return res.status(400).json({ 'message': 'Employee ID required.' });
-
-    const employee = await Employee.findOne({ _id: req.params.id }).exec();
-    if (!employee) {
-        return res.status(204).json({ "message": `No employee matches ID ${req.params.id}.` });
-    }
-    res.json(employee);
-}
-
-module.exports = {
-    getAllEmployees,
-    createNewEmployee,
-    updateEmployee,
-    deleteEmployee,
-    getEmployee
-}
-*/
